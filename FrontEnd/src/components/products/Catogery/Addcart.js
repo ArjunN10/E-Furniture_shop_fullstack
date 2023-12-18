f8d9abb21fb28f46304196407cf17292feaf44f7
ARJUN N <138310139+ArjunN10@users.noreply.github.com>
2023-12-18 14:53:41 +0530

    View Cart section

--- FrontEnd/src/components/products/Catogery/Addcart.js
+++ FrontEnd/src/components/products/Catogery/Addcart.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
   MDBBtn,
   MDBCard,
@@ -10,9 +10,7 @@ import {
   MDBRow,
   MDBTypography,
 } from "mdb-react-ui-kit";
-import { useNavigate, useParams } from "react-router-dom";
-import { Mycontext } from "../../../context/Context";
-import Counter from "../Counter";
+import { useNavigate,} from "react-router-dom";
 import Footer from "../../Footer";
 import { Axios } from "../../../App";
 import toast from "react-hot-toast";
@@ -20,19 +18,21 @@ import toast from "react-hot-toast";
 function Addcart() {
   const [addcart, setaddcart ] = useState([])
   const navigate = useNavigate();
+// const {id}=useParams()
+const userid=localStorage.getItem("UserId")
+
+
 
   const handleBackToShopping = () => {
     navigate("/");
   };
 
-const {id}=useParams()
-
-const userid=localStorage.getItem("UserId")
 const FetchCartproducts=async()=>{
   try {
     const response=await Axios.get(`/api/users/${userid}/cart`)
+    console.log("res",response)
     if(response.status === 200){
-      setaddcart(response.data.data.cart)
+      setaddcart(response.data.data)
     }
   } catch (error) {
     console.log(error)
@@ -43,18 +43,19 @@ useEffect(()=>{
   FetchCartproducts() 
 },[])
 
-
+console.log("----------------",addcart)
 
 // Handle Product Quantity
 
-const handleQuantity=async(cartId,quantityChange)=>{
-  const data={id:cartId,quantityChange}
+const handleQuantity=async(id,quantityChange)=>{
+  const data={id,quantityChange}
   try {
-    await Axios.put(`/api/users/${id}/cart`,data)
+    await Axios.put(`/api/users/${userid}/cart`,data)
     const response=await Axios.get(`/api/users/${userid}/cart`)
+    console.log(response)
     if(response.status === 200){
       return FetchCartproducts()
-    }
+    } 
   } catch (error) {
     toast.error(error) 
   }
@@ -62,9 +63,9 @@ const handleQuantity=async(cartId,quantityChange)=>{
 
 
 // Handle Product Remove
-const handleRemoveItem=async(ItemId)=>{
+const handleRemoveItem=async(itemId)=>{
   try {
-    const response=await Axios.delete(`/api/users/${id}/cart/${ItemId}`)
+    const response=await Axios.delete(`/api/users/${userid}/cart/${itemId}`)
     if (response.status === 200) {
       toast.success(response.data.message)
       return FetchCartproducts()
@@ -73,6 +74,7 @@ const handleRemoveItem=async(ItemId)=>{
     console.error(error);
     toast.error('Error removing product from the cart');
   }
+
 }
 
 
@@ -91,6 +93,10 @@ const handleCheckout=async()=>{
   }
 }
 
+const calculateTotal = (cartItems) => {
+  return cartItems.reduce((total, item) => total + item.productsId.price * item.quantity, 0).toFixed(2);
+};
+
 
 
   return (
@@ -106,22 +112,24 @@ const handleCheckout=async()=>{
                 >
                   Cart products
                 </MDBTypography>
+                {/* {console.log(addcart)} */}
                 { addcart.map((item) => (
-                  <div className="d-flex align-items-center mb-4" key={item.productsId._id}>
+                  <div className="d-flex align-items-center mb-4" key={item.productsId}>
+                    {/* {console.log(item.productsId)} */}
                     <div className="flex-shrink-0">
                       <MDBCardImage
                         src={item.productsId.image}
                         fluid
                         style={{ maxWidth: "150px" }}
                         alt={item.productsId.title}
-                      />
+                        />
                     </div>
 
                     <div className="flex-grow-1 ms-3">
                       <a
                         href="#!"
                         className="float-end text-black"
-                        onClick={() =>  handleRemoveItem(item._id)}
+                        onClick={() =>  handleRemoveItem(item.productsId._id)}
                       >
                         <MDBIcon fas icon="times" />
                       </a>
@@ -133,15 +141,15 @@ const handleCheckout=async()=>{
                         <MDBBtn
                                 style={{ border: "1px" }}
                                 className="minus mx-3 "
-                                onClick={() => handleQuantity(item.productsId._id, -1)}
-                              >
+                                onClick={() => handleQuantity(item._id, -1)}
+                                >
                                 <MDBIcon fas icon="minus" />
                               </MDBBtn>
                              <span className="me-4">{item.quantity}</span>
                               <MDBBtn
                                 className="plus"
                                 style={{ border: "1px" }}
-                               onClick={() => handleQuantity(item.productsId._id, 1)}
+                               onClick={() => handleQuantity(item._id, 1)}
                               >
                             <MDBIcon fas icon="plus" />
                           </MDBBtn>
@@ -156,7 +164,9 @@ const handleCheckout=async()=>{
           <MDBCol lg="5">
             <form className="mb-4">
               <MDBTypography tag="h5" className="fw-bold mb-4">
-                <p className="text-center fw-bold text-uppercase">GRAND TOTAL: ${}</p>
+              <p className="text-center fw-bold text-uppercase">
+                  GRAND TOTAL: ${calculateTotal(addcart)}
+                </p>
               </MDBTypography>
               <MDBBtn
                 onClick={() => handleCheckout()}
