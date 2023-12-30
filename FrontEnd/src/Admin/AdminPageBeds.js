ab46be7618156871c20901f9be8ec3c36e63b938
ARJUN N <138310139+ArjunN10@users.noreply.github.com>
2023-12-30 12:06:41 +0530

    Admin category

--- FrontEnd/src/Admin/AdminPageBeds.js
+++ FrontEnd/src/Admin/AdminPageBeds.js
@@ -18,38 +18,67 @@ function AdminPageSofas() {
     const navigate=useNavigate()
    
 
-    useEffect(() => {
-      const productBycategory=async()=>{
-        try {
-          const jwtToken = {
-            headers: {
-              Authorization: `${localStorage.getItem("Admin jwt")}`,
-            },
-          };
-          // console.log(jwtToken)
-          const response = await axios.get(`http://localhost:3003/api/admin/products/category/${categoryname}`,jwtToken)
-          console.log(response)
-          if(response.status === 200){
-            setproducts(response.data.data) 
-          
-             console.log(response.data.data) 
-          }
-        } catch (error) {
-          console.log("error :",error)
-          toast.error(error)
+
+    const productBycategory=async()=>{
+      try {
+        const jwtToken = {
+          headers: {
+            Authorization: `${localStorage.getItem("Admin jwt")}`,
+          },
+        };
+        // console.log(jwtToken)
+        const response = await axios.get(`http://localhost:3003/api/admin/products/category/${categoryname}`,jwtToken)
+        console.log(response)
+        if(response.status === 200){
+          setproducts(response.data.data) 
+        
+           console.log(response.data.data) 
         }
+      } catch (error) {
+        console.log("error :",error)
+        toast.error(error)
       }
+    }
+
+
+    useEffect(() => {
+     
       productBycategory()
       
           window.scrollTo(0, 0);
         }, []);
 
+        const handleRemove = async (productId) => {
+          try {
+            const jwtToken = {
+              headers: {
+                Authorization: `${localStorage.getItem("Admin jwt")}`,
+              },
+            };
+            
+            const response = await axios.delete(`http://localhost:3003/api/admin/products`,{...jwtToken,data:{ productId }})
+            // console.log(response)
+            if (response.status === 200) {
+              setproducts(response.data.data);
+              toast.success("Product deleted successfully!");
+             productBycategory()
+              
+            }
+          } catch (error) {
+            console.error("Error deleting product:", error);
+            toast.error("Failed to delete product.");
+          }
+        };
+
 
 
   return (
-    <div>
-       <MDBTable responsive className='caption-top'>
-<caption>TOTAL PRODUCTS:{}</caption>
+    <>
+    <div >
+<MDBTable responsive className='caption-top '>
+<caption className='ps-5 pt-5'><h4>
+TOTAL PRODUCTS: {products?.length || 0}
+  </h4></caption>
       <MDBTableHead>
         <tr>
           <th scope='col'>ID/</th>
@@ -61,22 +90,21 @@ function AdminPageSofas() {
           {/* <th scope='col'>Offer Price</th> */}
         </tr>
       </MDBTableHead>
-      {products.map((item,index)=>
      
       <MDBTableBody>
-        <tr>
+      {products &&products.map((item)=>
+        <tr key={item._id}>
           <td>{item._id}</td>
           <td>
             <div className='d-flex align-items-center'>
               <img
                 src={item.image}
-                alt=''
+                alt='Photos'
                 style={{ width: '45px', height: '45px' }}
                 className='rounded-circle'
               />
               <div className='ms-3'>
                 <p className='fw-bold mb-1'>{item.title}</p>
-          
               </div>
             </div>
           </td>
@@ -96,7 +124,7 @@ function AdminPageSofas() {
             <MDBBtn 
             color='link'
             rounded size='sm'
-            onClick={()=>navigate('/adminedit')}>
+            onClick={()=>navigate(`/adminhome/adminedit/${item._id}`)}>
               Edit
             </MDBBtn>
           </td>
@@ -104,17 +132,18 @@ function AdminPageSofas() {
             <MDBBtn 
             color='danger'
             rounded size='sm'
-            onClick={()=>setproducts(p=>p.filter((a,i)=>i!=index))}
-            
-            >
+            // onClick={()=>setproducts(p=>p.filter((a,i)=>i!=index))}
+            onClick={() => handleRemove(item._id)}            >
             Delete
             </MDBBtn>
           </td>
-               </tr>
-      </MDBTableBody>
-       )}
+        </tr>
+      
+      )}
+       </MDBTableBody>
     </MDBTable>
     </div>
+    </>
   )
 }
 
--- FrontEnd/src/Admin/AdminPageChairs.js
+++ FrontEnd/src/Admin/AdminPageChairs.js
@@ -18,27 +18,31 @@ function AdminPageSofas() {
     const navigate=useNavigate()
    
 
-    useEffect(() => {
-      const productBycategory=async()=>{
-        try {
-          const jwtToken = {
-            headers: {
-              Authorization: `${localStorage.getItem("Admin jwt")}`,
-            },
-          };
-          // console.log(jwtToken)
-          const response = await axios.get(`http://localhost:3003/api/admin/products/category/${categoryname}`,jwtToken)
-          console.log(response)
-          if(response.status === 200){
-            setproducts(response.data.data) 
-          
-             console.log(response.data.data) 
-          }
-        } catch (error) {
-          console.log("error :",error)
-          toast.error(error)
+
+    const productBycategory=async()=>{
+      try {
+        const jwtToken = {
+          headers: {
+            Authorization: `${localStorage.getItem("Admin jwt")}`,
+          },
+        };
+        // console.log(jwtToken)
+        const response = await axios.get(`http://localhost:3003/api/admin/products/category/${categoryname}`,jwtToken)
+        console.log(response)
+        if(response.status === 200){
+          setproducts(response.data.data) 
+        
+           console.log(response.data.data) 
         }
+      } catch (error) {
+        console.log("error :",error)
+        toast.error(error)
       }
+    }
+
+
+    useEffect(() => {
+      
       productBycategory()
       
           window.scrollTo(0, 0);
@@ -46,76 +50,103 @@ function AdminPageSofas() {
 
 
 
-  return (
-    <div>
-       <MDBTable responsive className='caption-top'>
-<caption>TOTAL PRODUCTS:{}</caption>
-      <MDBTableHead>
-        <tr>
-          <th scope='col'>ID/</th>
-          <th scope='col'>Products</th>
-          <th scope='col'>Description</th>
-          <th scope='col'>Availability</th>
-          <th scope='col'>Type</th>
-          <th scope='col'>Price</th>
-          {/* <th scope='col'>Offer Price</th> */}
-        </tr>
-      </MDBTableHead>
-      {products.map((item,index)=>
-     
-      <MDBTableBody>
-        <tr>
-          <td>{item._id}</td>
-          <td>
-            <div className='d-flex align-items-center'>
-              <img
-                src={item.image}
-                alt=''
-                style={{ width: '45px', height: '45px' }}
-                className='rounded-circle'
-              />
-              <div className='ms-3'>
-                <p className='fw-bold mb-1'>{item.title}</p>
-          
-              </div>
-            </div>
-          </td>
-          <td>
-            <p className='fw-normal mb-1'>{item.description}</p>
-    
-          </td>
-          <td>
-            <MDBBadge color='success' pill>
-              Available
-            </MDBBadge>
-          </td>
-          <td>{item.category}</td>
-          <td>{item.price}</td>
-          {/* <td>{item.price2}</td> */}
-          <td>
-            <MDBBtn 
-            color='link'
-            rounded size='sm'
-            onClick={()=>navigate('/adminedit')}>
-              Edit
-            </MDBBtn>
-          </td>
-          <td>
-            <MDBBtn 
-            color='danger'
-            rounded size='sm'
-            onClick={()=>setproducts(p=>p.filter((a,i)=>i!=index))}
+        const handleRemove = async (productId) => {
+          try {
+            const jwtToken = {
+              headers: {
+                Authorization: `${localStorage.getItem("Admin jwt")}`,
+              },
+            };
             
-            >
-            Delete
-            </MDBBtn>
-          </td>
-               </tr>
-      </MDBTableBody>
-       )}
-    </MDBTable>
-    </div>
-  )
-}
+            const response = await axios.delete(`http://localhost:3003/api/admin/products`,{...jwtToken,data:{ productId }})
+            // console.log(response)
+            if (response.status === 200) {
+              setproducts(response.data.data);
+              toast.success("Product deleted successfully!");
+              productBycategory()
+              
+            }
+          } catch (error) {
+            console.error("Error deleting product:", error);
+            toast.error("Failed to delete product.");
+          }
+        };
+
+
+
+        return (
+          <>
+          <div >
+      <MDBTable responsive className='caption-top '>
+      <caption className='ps-5 pt-5'><h4>
+      TOTAL PRODUCTS: {products?.length || 0}
+        </h4></caption>
+            <MDBTableHead>
+              <tr>
+                <th scope='col'>ID/</th>
+                <th scope='col'>Products</th>
+                <th scope='col'>Description</th>
+                <th scope='col'>Availability</th>
+                <th scope='col'>Type</th>
+                <th scope='col'>Price</th>
+                {/* <th scope='col'>Offer Price</th> */}
+              </tr>
+            </MDBTableHead>
+           
+            <MDBTableBody>
+            {products &&products.map((item)=>
+              <tr key={item._id}>
+                <td>{item._id}</td>
+                <td>
+                  <div className='d-flex align-items-center'>
+                    <img
+                      src={item.image}
+                      alt='Photos'
+                      style={{ width: '45px', height: '45px' }}
+                      className='rounded-circle'
+                    />
+                    <div className='ms-3'>
+                      <p className='fw-bold mb-1'>{item.title}</p>
+                    </div>
+                  </div>
+                </td>
+                <td>
+                  <p className='fw-normal mb-1'>{item.description}</p>
+          
+                </td>
+                <td>
+                  <MDBBadge color='success' pill>
+                    Available
+                  </MDBBadge>
+                </td>
+                <td>{item.category}</td>
+                <td>{item.price}</td>
+                {/* <td>{item.price2}</td> */}
+                <td>
+                  <MDBBtn 
+                  color='link'
+                  rounded size='sm'
+                  onClick={()=>navigate(`/adminhome/adminedit/${item._id}`)}>
+                    Edit
+                  </MDBBtn>
+                </td>
+                <td>
+                  <MDBBtn 
+                  color='danger'
+                  rounded size='sm'
+                  // onClick={()=>setproducts(p=>p.filter((a,i)=>i!=index))}
+                  onClick={() => handleRemove(item._id)}            >
+                  Delete
+                  </MDBBtn>
+                </td>
+              </tr>
+            
+            )}
+             </MDBTableBody>
+          </MDBTable>
+          </div>
+          </>
+        )
+      }
 
 export default AdminPageSofas
